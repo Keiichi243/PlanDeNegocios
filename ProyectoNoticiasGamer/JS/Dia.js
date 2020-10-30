53dcd2b30034bae880bd96aa6d94f004cf732fd8
Mauricio <43255087+Mauricio1026@users.noreply.github.com>
2020-10-30 00:45:54 -0500

    Actualización Pagina de inicio
    Se realizo la actualizacion de la pagina de inicio.

---
+++ ProyectoNoticiasGamer/JS/Dia.js
@@ -0,0 +1,11 @@
+function dia_de_hoy(){
+    var hoy = new Date();
+    //document.write(hoy);
+    document.write(hoy.toDateString());
+}
+/*
+function escribe_nombre(nombre){
+    document.write(nombre);
+}
+var mi_nombre;
+mi_nombre=prompt("Introduzca su nombre");*/
